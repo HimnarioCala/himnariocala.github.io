dc1ae254c95d6de6a3bc55bd936f7f90860e8ace
himnariocala <99310785+himnariocala@users.noreply.github.com>
2023-04-13 23:40:09 -0400

    invitacion2

--- js/app.js
+++ js/app.js
@@ -1,15 +1,8 @@
 const buttonwhatsapp = document.querySelector("#buttonwhatsapp");
 
 buttonwhatsapp.addEventListener("click", () => {
-  // https://api.whatsapp.com/send?phone=+59173251894&text=Sistema%20IEBS%20Nacional%20Ayuda"
-  var telefono = "+59173251894";
-  
-  // Mensaje que se enviará
-  var mensaje = "Hola, ¿cómo estás?";
-  
-  // URL personalizada de WhatsApp
-  var url = "whatsapp://send?phone=" + telefono + "&text=" + encodeURIComponent(mensaje);
-  
-  // Intentar abrir la aplicación de WhatsApp directamente
+  const cellular = "+59173251894";
+  const message = "Himnario CALA - Diseño gráfico (Invitación)";
+  const url = `whatsapp://send?phone=${cellular}&text=${encodeURIComponent(message)}`
   window.location.href = url;
 });
